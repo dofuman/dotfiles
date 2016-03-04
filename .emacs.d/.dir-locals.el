18efab5d7f4c68b0a821cfb7bd421457cffc1419
dofuman <dofuman@example.com>
2016-03-04 19:04:30 +0900

    flycheck用のinclude path設定ファイル追加

---
+++ .emacs.d/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c++-mode .
+		   ( (flycheck-gcc-include-path .
+										("/usr/include/pcl-1.7/"
+										 "./src/") ) )
+         ))
