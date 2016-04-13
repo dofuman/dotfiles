871664755f7692cd4ca53aad0daaa3498229f271
dofuman <odz70j0z3883d6c@gmail.com>
2016-04-13 12:58:08 +0900

    add pdf,arduino,cuda,gnuplot,web,yaml-mode

--- .emacs.d/buffer/00_lang_encoding.el
+++
@@ -1,5 +0,0 @@
-;;
-;; language encoding config
-;;
-;; 極力UTF-8とする
-(prefer-coding-system 'utf-8)
--- .emacs.d/buffer/09_font.el
+++
@@ -1,9 +0,0 @@
-;; font config
-(set-face-attribute 'default nil :family "Menlo" :height 140)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
-(add-to-list 'face-font-rescale-alist
-             '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
-
-
