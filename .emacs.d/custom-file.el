7f753c0998fcac1c34f09e92196cceb9b1aa20fa
nishidalab <nishidalab@example.com>
2016-07-19 19:46:08 +0900

    change color-theme and python's indent

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -1,8 +1,33 @@
+;;; カスタマイズできる項目！
+;;; これらはload-themeの前に配置すること
+;; fringeを背景から目立たせる
+;; (setq solarized-distinct-fringe-background t)
+;;
+;; mode-lineを目立たせる(Fig3)
+;; (setq solarized-high-contrast-mode-line t)
+;;
+;; bold度を減らす
+;; (setq solarized-use-less-bold t)
+;;
+;; italicを増やす
+;; (setq solarized-use-more-italic t)
+;;
+;; インジケータの色を減らす (git-gutter, flycheckなど)
+;; (setq solarized-emphasize-indicators nil)
+;;
+;; orgの見出し行の文字の大きさを変えない
+;; (setq solarized-scale-org-headlines nil)
+;;
+;; フォントサイズを変更しない
+;; (setq solarized-height-minus-1 1)
+;; (setq solarized-height-plus-1 1)
+;; (setq solarized-height-plus-2 1)
+;; (setq solarized-height-plus-3 1)
+;; (setq solarized-height-plus-4 1)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/themes")
-(load-theme 'misterioso t)
+(load-theme 'solarized-dark t)
 (require 'platform-p)
 ;; 背景を透明にする(透明度合いは 95/100)
 ;(set-frame-parameter nil 'alpha 95)
 ;;; カッコのハイライト
 (show-paren-mode t)
-
--- .emacs.d/inits/20_python.el
+++ .emacs.d/inits/20_python.el
@@ -23,6 +23,9 @@
 ;; (eval-after-load "python"
 ;;   '(define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer))
 ;; (add-hook 'jedi-mode-hook 'jedi-direx:setup)
+;;
+;;
+;;
 (add-hook 'python-mode-hook
         (lambda ()
             (setq-default indent-tabs-mode t)
