0065fb3792c11365e6d7b63513176071bd3058ef
dofuman <dofuman@example.com>
2016-12-21 15:29:30 +0100

    update 30_yasnippet.el
    * 新規スニペットの保存方法をコメントに記述

--- .emacs.d/inits/30_yasnippet.el
+++ .emacs.d/inits/30_yasnippet.el
@@ -12,15 +12,15 @@
   '(progn
      ;;companyと競合するのでyasnippetのフィールド移動は"C-i"のみにする
      (define-key yas-keymap (kbd "<tab>")nil)
-     (yas-global-mode 1))) 
+     (yas-global-mode 1)))
 
 (defalias 'yas/get-snippet-tables 'yas--get-snippet-tables)
 (defalias 'yas/table-hash 'yas--table-hash)
- 
+
 ;; 既存スニペットを挿入する
 (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 ;; 新規スニペットを作成するバッファを用意する
+;; C-c C-cで保存.
 (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 ;; 既存スニペットを閲覧・編集する
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
-
