077e86deb64ceb25b8ab7b39cc9ba06e114d4f04
dofuman <dofuman@example.com>
2016-12-21 15:31:16 +0100

    update costome-file.el
    * フォントの調整を削除.
    * emacsの背景を透過するように変更.

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -1,29 +1,5 @@
-;;; カスタマイズできる項目！
-;;; これらはload-themeの前に配置すること
-;; fringeを背景から目立たせる
-;; (setq solarized-distinct-fringe-background t)
-;;
-;; mode-lineを目立たせる(Fig3)
-;; (setq solarized-high-contrast-mode-line t)
-;;
-;; bold度を減らす
-;; (setq solarized-use-less-bold t)
-;;
-;; italicを増やす
-;; (setq solarized-use-more-italic t)
-;;
-;; インジケータの色を減らす (git-gutter, flycheckなど)
-;; (setq solarized-emphasize-indicators nil)
-;;
-;; orgの見出し行の文字の大きさを変えない
-;; (setq solarized-scale-org-headlines nil)
-;;
-;; フォントサイズを変更しない
-;; (setq solarized-height-minus-1 1)
-;; (setq solarized-height-plus-1 1)
-;; (setq solarized-height-plus-2 1)
-;; (setq solarized-height-plus-3 1)
-;; (setq solarized-height-plus-4 1)
+;; custom emacs theme 
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/atom-one-dark-theme/")
 (load-theme 'atom-one-dark t)
 (require 'platform-p)
@@ -31,3 +7,10 @@
 ;(set-frame-parameter nil 'alpha 95)
 ;;; カッコのハイライト
 (show-paren-mode t)
+
+;; transparent background
+
+(set-frame-parameter (selected-frame) 'alpha '(85 85))
+(add-to-list 'default-frame-alist '(alpha 85 85))
+
+
