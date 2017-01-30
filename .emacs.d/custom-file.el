e3fffdc7a025304061e8e89a665c1741984eb59d
dofuman <q595202s@mail.kyutech.jp>
2017-01-30 16:33:37 +0100

    modified:   .emacs.d/custom-file.el
    ＊　透過設定など

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -10,7 +10,7 @@
 
 ;; transparent background
 
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
+(set-frame-parameter (selected-frame) 'alpha '(98 98))
+(add-to-list 'default-frame-alist '(alpha 98 98))
 
 
