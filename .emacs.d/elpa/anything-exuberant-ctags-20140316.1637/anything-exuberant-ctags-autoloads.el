52b4696e3a7de63aa6adb4e37baa85bcea5e3cd7
dofuman <q595202s@mail.kyutech.jp>
2017-01-30 16:34:48 +0100

    updated:	各.elファイルを更新した
    ＊　emacs25.1に対応するためelpaパッケージを更新した

--- .emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags-autoloads.el
+++ .emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags-autoloads.el
@@ -1,11 +1,11 @@
 ;;; anything-exuberant-ctags-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "anything-exuberant-ctags" "anything-exuberant-ctags.el"
-;;;;;;  (22168 19786 175763 693000))
-;;; Generated autoloads from anything-exuberant-ctags.el
+;;;### (autoloads nil "anything-exuberant-ctags" "../../../../../.emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags.el"
+;;;;;;  "bd37d51c2a99429f812e96dedacbe4ab")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags.el
 
 (autoload 'anything-exuberant-ctags-select "anything-exuberant-ctags" "\
 Tag jump using `anything'.
@@ -34,6 +34,12 @@ Toggle tag file cache directory status.
 \(fn)" t nil)
 
 (defvar anything-c-source-exuberant-ctags-select '((name . "Exuberant ctags") (header-name . anything-source-exuberant-ctags-header-name) (init . anything-exuberant-ctags-create-buffer) (candidates-in-buffer) (get-line . anything-exuberant-ctags-get-line) (action ("Goto the location" . anything-exuberant-ctags-goto-location)) (candidate-number-limit . 9999) (candidate-transformer lambda (candidates) (anything-exuberant-ctags-transformer candidates))))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/anything-exuberant-ctags-20140316.1637/anything-exuberant-ctags.el")
+;;;;;;  (22671 23314 483916 711000))
 
 ;;;***
 
--- .emacs.d/inits/20_tex.el
+++ .emacs.d/inits/20_tex.el
@@ -25,33 +25,33 @@
 (add-hook 'foo-mode-hook 'ac-l-setup)
 
 (require 'platform-p)
-(when platform-linux-p ; for GNU/Linux
-;;; inverse search
-  (require 'dbus)
+;; (when platform-linux-p ; for GNU/Linux
+;; ;;; inverse search
+;;   (require 'dbus)
   
-  (defun un-urlify (fname-or-url)
-	"A trivial function that replaces a prefix of file:/// with just /."
-	(if (string= (substring fname-or-url 0 8) "file:///")
-		(substring fname-or-url 7)
-	  fname-or-url))
+;;   (defun un-urlify (fname-or-url)
+;; 	"A trivial function that replaces a prefix of file:/// with just /."
+;; 	(if (string= (substring fname-or-url 0 8) "file:///")
+;; 		(substring fname-or-url 7)
+;; 	  fname-or-url))
   
-  (defun evince-inverse-search (file linecol &rest ignored)
-	(let* ((fname (un-urlify file))
-		   (buf (find-file fname))
-		   (line (car linecol))
-		   (col (cadr linecol)))
-	  (if (null buf)
-		  (message "[Synctex]: %s is not opened..." fname)
-		(switch-to-buffer buf)
-		(goto-line (car linecol))
-		(unless (= col -1)
-		  (move-to-column col)))))
+;;   (defun evince-inverse-search (file linecol &rest ignored)
+;; 	(let* ((fname (un-urlify file))
+;; 		   (buf (find-file fname))
+;; 		   (line (car linecol))
+;; 		   (col (cadr linecol)))
+;; 	  (if (null buf)
+;; 		  (message "[Synctex]: %s is not opened..." fname)
+;; 		(switch-to-buffer buf)
+;; 		(goto-line (car linecol))
+;; 		(unless (= col -1)
+;; 		  (move-to-column col)))))
   
-  (dbus-register-signal
-   :session nil "/org/gnome/evince/Window/0"
-   "org.gnome.evince.Window" "SyncSource"
-   'evince-inverse-search)
-  )
+;;   (dbus-register-signal
+;;    :session nil "/org/gnome/evince/Window/0"
+;;    "org.gnome.evince.Window" "SyncSource"
+;;    'evince-inverse-search)
+;;   )
 
 ;;; popwin(for yatex)
 (require 'popwin-yatex)
